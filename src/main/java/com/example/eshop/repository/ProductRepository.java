6bc35a3973a52061077504d184c0ee6e5bf7ffe2
kasiupav <147624779+kasiupav@users.noreply.github.com>
2026-03-18 23:28:28 +0100

    Update ProductRepository.java

--- src/main/java/com/example/eshop/repository/ProductRepository.java
+++ src/main/java/com/example/eshop/repository/ProductRepository.java
@@ -1,1 +1,9 @@
+package com.example.eshop.repository;
 
+import com.example.eshop.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
