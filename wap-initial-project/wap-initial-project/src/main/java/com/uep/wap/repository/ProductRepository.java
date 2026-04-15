663509195dde8b488783a5ab6d2f6705de196ca7
ulyana560 <148351659+ulyana560@users.noreply.github.com>
2026-04-15 19:50:55 +0200

    Delete wap-initial-project/wap-initial-project directory

--- wap-initial-project/wap-initial-project/src/main/java/com/uep/wap/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uep.wap.repository;
-
-import com.uep.wap.model.Product;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
-}
